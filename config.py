1670a02478c98290c8e18caea7a007b8a5d384ed
Ruochen Jing <jing2867612710@outlook.com>
2026-07-11 17:42:37 +0800

    更新 config.py

--- config.py
+++ config.py
@@ -262,4 +262,4 @@ SMTP_PORT = int(_clean_env_value(os.environ.get("SMTP_PORT", "465")))        # �
 SENDER_EMAIL = _clean_env_value(os.environ.get("SENDER_EMAIL", ""))
 EMAIL_PASSWORD = _clean_env_value(os.environ.get("EMAIL_PASSWORD", ""))      # 这里填授权码
 RECEIVER_EMAIL = _clean_env_value(os.environ.get("RECEIVER_EMAIL", ""))
-KVDB_BUCKET = _clean_env_value(os.environ.get("KVDB_BUCKET", ""))
+KVDB_BUCKET = _clean_env_value(os.environ.get("KVDB_BUCKET", "EFYeTmNsH74hkM8ZJrLoxv"))
